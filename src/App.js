32884e16dc83c03cd10a3a0494f50bca56555f51
sinta amelika <sinta.amelika@gmail.com>
2020-11-03 13:06:07 +0700

    display list of photos from backend API respond

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  
-  return (
-    <div className="App">
+async function fetchData() {
+  const response = await fetch('http://localhost:8080/photos_public');
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  const photos = await response.json();
+  return photos;
+}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {photos: []};
+  }
+
+
+
+  componentDidMount(){
+    fetchData().then(photos => {
+      console.log(photos);
+      if(photos && photos.items){
+        const list = photos.items.map((item, index) =>
+
+          <li key={index}>{item.title}
+            <img src={item.media.m} alt={item.title}></img>
+          </li>
+        );
+        this.setState({
+          photos: list
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+  }
+  render() {
+    return <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -16,14 +53,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 
+          Learn React {this.state.tes}
         </a>
-        <ul>
-        
-        </ul>
+        <ul>{this.state.photos}</ul>
       </header>
     </div>
-  );
+  }
 }
-
 export default App;
